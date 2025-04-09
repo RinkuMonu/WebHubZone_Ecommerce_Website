74753985cda326be305c7e7155c9a518f2d466ff
RinkuMonu <rinkuyadav9460@gmail.com>
2025-04-09 18:02:23 +0530

    changes successfully

--- src/pages/AddresShiping.tsx
+++ src/pages/AddresShiping.tsx
@@ -165,10 +165,11 @@ function AddressShiping({ cartItems }) {
       );
 
       
-      console.log("responseeeeee", response.data?.payment_url);
+      console.log("responseeeeee", response.data?.payment_link);
+      console.log("responseeeeeerr", response.data?.data?.payment_link);
 
 
-      const paymentLink = response.data?.payment_url;
+      const paymentLink = response.data?.data?.payment_link;
       console.log(paymentLink)
       const cleanedUrl = paymentLink.replace(/\\/g, "");
 
